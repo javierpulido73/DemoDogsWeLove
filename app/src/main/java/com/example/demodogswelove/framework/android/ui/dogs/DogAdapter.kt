cd9e9f7c17e54363f3fed7d242011122d5ad5228
Francisco Pulido Fabian <jpulido@nisum.com>
2022-06-18 10:45:39 -0500

    feat(adapter): moddify age field

--- app/src/main/java/com/example/demodogswelove/framework/android/ui/dogs/DogAdapter.kt
+++ app/src/main/java/com/example/demodogswelove/framework/android/ui/dogs/DogAdapter.kt
@@ -1,10 +1,12 @@
 package com.example.demodogswelove.framework.android.ui.dogs
 
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions.withCrossFade
 import com.bumptech.glide.request.RequestOptions
+import com.example.demodogswelove.R
 import com.example.demodogswelove.application.model.DogModel
 import com.example.demodogswelove.databinding.ItemDogViewBinding
 import com.example.demodogswelove.framework.glide.GlideInstance
@@ -14,22 +16,22 @@ import com.example.demodogswelove.framework.glide.GlideInstance
  */
 class DogAdapter(private val dogs: List<DogModel>) :
     RecyclerView.Adapter<RecyclerView.ViewHolder>() {
-    private class DogViewHolder(val dataBinding: ItemDogViewBinding) :
+    private class DogViewHolder(val dataBinding: ItemDogViewBinding, val context: Context) :
         RecyclerView.ViewHolder(dataBinding.root)
 
     override fun getItemCount(): Int = dogs.size
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
         if (holder is DogViewHolder) {
+
             holder.dataBinding.apply {
                 dogName = dogs[position].dogName
                 dogDescription = dogs[position].description
-                dogAge = dogs[position].age
-                GlideInstance.imageBaseUrl?.let {
-                    GlideInstance.glide.load(dogs[position].url)
-                        .apply(RequestOptions.centerCropTransform()).transition(withCrossFade())
-                        .into(imageView)
-                }
+                dogAge = holder.context.getString(R.string.almost_age, dogs[position].age.toString())
+                GlideInstance.glide.load(dogs[position].image)
+                    .apply(RequestOptions.centerCropTransform())
+                    .transition(withCrossFade())
+                    .into(imageView)
             }
         }
     }
@@ -40,6 +42,6 @@ class DogAdapter(private val dogs: List<DogModel>) :
                 LayoutInflater.from(parent.context),
                 parent,
                 false
-            )
+            ), parent.context
         )
 }
